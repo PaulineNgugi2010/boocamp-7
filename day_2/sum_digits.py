289728fbfe2b05b2a3ee850c10f01b6e1c153661
Pauline Ngugi <gathoni.pauline2010@gmail.com>
2016-04-27 08:16:47 +0300

    sum_digits

--- day_2/sum_digits.py
+++ day_2/sum_digits.py
@@ -5,14 +5,12 @@ def sum_digits(A):
 			in the list e.g. [10, 30, 45] 
 			should return 1 + 0 + 3 + 4 + 5
 	'''
-	counter = 0
-	
-	for i in A:
-		p = str(i)
-		for j in p:
-			counter += int(j)
-	
-	return counter
-
+	total = 0
+	for i  in A:
+		b = str(i)
+		for i in b:
+			num = int(i)
+			total += num
+	return total
 
 print sum_digits([10, 30, 45])	
