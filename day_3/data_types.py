34578457f1ac24c20b562c231fbe5ae8195397a6
Pauline Ngugi <gathoni.pauline2010@gmail.com>
2016-04-27 10:37:54 +0300

    data_type

---
+++ day_3/data_types.py
@@ -0,0 +1,28 @@
+def data_type(x):
+	'''
+	Takes an argument, x:
+		-For a integer, return x**2
+		-For a float, return x/2
+		-For a string, return x/2
+		-For a boolean, return "boolean"
+		-For a long, return  squareroot(x)
+	'''
+	if type(x) == int:
+		return x **2
+	elif type(x) ==float:
+		return x / 2
+	elif type(x) == str:
+		return "hello " + x
+	elif type(x) == long:
+		return "long"
+	elif type(x) == bool:
+		return "boolean"
+
+	else:
+	 return "No category found"
+
+print data_type(5)
+print data_type(4.5)
+print data_type(500000L)
+print data_type(5>2)
+print data_type("Pauline")
